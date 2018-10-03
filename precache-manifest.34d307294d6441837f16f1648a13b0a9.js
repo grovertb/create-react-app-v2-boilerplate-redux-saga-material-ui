1fb70b430f69de13bceeaa99a14097fe562bc0f5
GroverTB <grover.jtb@gmail.com>
2018-10-03 18:07:11 -0500

    Updates

--- precache-manifest.34d307294d6441837f16f1648a13b0a9.js
+++ precache-manifest.34d307294d6441837f16f1648a13b0a9.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/create-react-app-v2-boilerplate-redux-saga-material-ui/static/js/runtime~main.d7c9d1fd.js"
   },
   {
-    "revision": "d34c785011288c65f0f8",
-    "url": "/create-react-app-v2-boilerplate-redux-saga-material-ui/static/js/main.d34c7850.chunk.js"
+    "revision": "4478fc05d0a80c3df961",
+    "url": "/create-react-app-v2-boilerplate-redux-saga-material-ui/static/js/main.4478fc05.chunk.js"
   },
   {
     "revision": "dd49f270104413fdb44e",
     "url": "/create-react-app-v2-boilerplate-redux-saga-material-ui/static/js/1.dd49f270.chunk.js"
   },
   {
-    "revision": "d34c785011288c65f0f8",
+    "revision": "4478fc05d0a80c3df961",
     "url": "/create-react-app-v2-boilerplate-redux-saga-material-ui/static/css/main.0a8a1d61.chunk.css"
   },
   {
-    "revision": "6867ff6e5f720a1938adb10a647092fb",
+    "revision": "1c160eb57eb3e66b7e4ccba1d2cd80bd",
     "url": "/create-react-app-v2-boilerplate-redux-saga-material-ui/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/create-react-app-v2-boilerplate-redux-saga-material-ui/precache-manifest.2b2a4dffeb67fd98493b04cac9a4d9a1.js"
+  "/create-react-app-v2-boilerplate-redux-saga-material-ui/precache-manifest.34d307294d6441837f16f1648a13b0a9.js"
 );
 
 workbox.clientsClaim();
