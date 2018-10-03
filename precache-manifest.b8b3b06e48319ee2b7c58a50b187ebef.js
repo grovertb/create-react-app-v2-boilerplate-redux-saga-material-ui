abc3b678346b2e7f50fc87978f5cf9536a498b7c
GroverTB <grover.jtb@gmail.com>
2018-10-03 11:44:33 -0500

    Updates

---
+++ precache-manifest.b8b3b06e48319ee2b7c58a50b187ebef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/create-react-app-v2-boilerplate-redux-saga-material-ui/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d7c9d1fdd98d00d5cac4",
+    "url": "/create-react-app-v2-boilerplate-redux-saga-material-ui/static/js/runtime~main.d7c9d1fd.js"
+  },
+  {
+    "revision": "6511e3184a7569c775b4",
+    "url": "/create-react-app-v2-boilerplate-redux-saga-material-ui/static/js/main.6511e318.chunk.js"
+  },
+  {
+    "revision": "d1d2c4f0b30b06b4dfeb",
+    "url": "/create-react-app-v2-boilerplate-redux-saga-material-ui/static/js/1.d1d2c4f0.chunk.js"
+  },
+  {
+    "revision": "6511e3184a7569c775b4",
+    "url": "/create-react-app-v2-boilerplate-redux-saga-material-ui/static/css/main.ac64a936.chunk.css"
+  },
+  {
+    "revision": "8510ebdfb495784f485ccb8435bb3b39",
+    "url": "/create-react-app-v2-boilerplate-redux-saga-material-ui/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/create-react-app-v2-boilerplate-redux-saga-material-ui/precache-manifest.b8b3b06e48319ee2b7c58a50b187ebef.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/create-react-app-v2-boilerplate-redux-saga-material-ui/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
